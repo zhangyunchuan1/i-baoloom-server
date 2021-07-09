e0e49285ef0c4faca689de6e80c11836c491aee4
zyc <1028580665@qq.com>
2021-07-09 10:35:49 +0800

    no message

--- config/config.default.js
+++ config/config.default.js
@@ -43,7 +43,7 @@ module.exports = appInfo => {
     client: {
       port: 6379,          // Redis port
       host: '127.0.0.1',   // Redis host
-      password: '',
+      password: '987654321yun.',
       db: 0,
     },
   };
