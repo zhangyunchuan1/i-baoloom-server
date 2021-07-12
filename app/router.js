848f32a74e081ba05f81b27e988d5e0a2ae92998
zyc <1028580665@qq.com>
2021-07-12 09:46:30 +0800

    33

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ const path = require('path');
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
+  router.post('/', controller.home.index);
   require('./router/user')(app);  //用户相关接口
   require('./router/article')(app);  //文章相关接口
   require('./router/color')(app);  //色卡相关接口
--- app/router/user.js
+++ app/router/user.js
@@ -1,6 +1,6 @@
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/user', controller.user.info);
+  router.post('/user', controller.user.info);
   router.post('/user/register', controller.user.register);
   router.post('/user/login', controller.user.login);
   router.post('/user/loginSendCode', controller.user.loginSendCode);
